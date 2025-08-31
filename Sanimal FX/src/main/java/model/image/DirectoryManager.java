1888957b6a4890bb67e73a62ac932ac26139d62b
Chris Schnaufer <schnaufer@email.arizona.edu>
2025-08-31 12:47:33 -0700

    Update DirectoryManager.java

--- Sanimal FX/src/main/java/model/image/DirectoryManager.java
+++ Sanimal FX/src/main/java/model/image/DirectoryManager.java
@@ -120,7 +120,7 @@ public class DirectoryManager
 			Arrays.sort(subFiles, new Comparator<File>() {
 										        @Override
 										        public int compare(File f1, File f2) {
-										            return f1.getName().localeCompare(f2.getName());
+										            return f1.getName().compareTo(f2.getName());
 										        }
 										    });
 
